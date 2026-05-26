d03353aa9f3d84fa9f384da02e2aed487c9bbb4d
Karan <37973085+karanavasthi24@users.noreply.github.com>
2026-05-26 21:11:13 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 2
+print(type(variable))
+variable = 23.3
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "karan"
+print(type(variable))
+variable = [2]
+print(type(variable))
