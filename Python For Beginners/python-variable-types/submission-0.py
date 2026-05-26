fa03c304ca3a02d3e47127906a5648a7a37becc2
Karan <37973085+karanavasthi24@users.noreply.github.com>
2026-05-26 21:10:14 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+
+integer_type = 3
+float_type = 2.3
+bool_type = False
+string_type = "karan"
+list_type = [1, "e"]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
